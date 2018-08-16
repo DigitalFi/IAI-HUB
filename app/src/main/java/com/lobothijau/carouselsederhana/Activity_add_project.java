48556f7d3d390b078a08e7ea4378981d543f1754
reza <rezasaputra.muhammad@yahoo.com>
2018-08-16 13:25:40 +0700

    fix minor

--- app/src/main/java/com/lobothijau/carouselsederhana/Activity_add_project.java
+++ app/src/main/java/com/lobothijau/carouselsederhana/Activity_add_project.java
@@ -1,7 +1,9 @@
 package com.lobothijau.carouselsederhana;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Activity_add_project extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class Activity_add_project extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_project);
+    }
+
+    public void Activity_add_project(View view) {
+
+        Intent intent = new Intent(Activity_add_project.this, Activity_project.class);
+        startActivity(intent);
+
     }
 }
